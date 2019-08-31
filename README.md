36eea83f1906a776a4143dc169aa53f1b6eecb7e
André Kunitz <andrekunitz@gmail.com>
2019-08-31 16:47:58 -0300

    Build a ToDo app usign classes

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # ES6
- Example of Javascript ES6 presented in the course *GoStack Starter* by *Rocketseat*.
+ Example of Javascript ES6 features presented in the course *GoStack Starter* by *Rocketseat*.
 
 ## What I Learned 
-- Package management with Yarn.
-- Transpile ES6+ to older versions with Babel.
-- Set up development environment with Babel Presets.
+- Package management with Yarn;
+- Set up development environment with Babel Presets;
+- Set up scripts to use with Yarn;
+- Transpile ES6+ to older versions with Babel;
+- Classes, constructors and static methods in ES6; 
--- main.js
+++ main.js
@@ -1,11 +1,37 @@
-alert('Teste')
-
-class Teste {
-  metodo() {
-
+class List {
+  constructor() {
+    this.data = [];
   }
 
-  outro() {
-
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
   }
 }
+
+class TodoList extends List {
+  constructor() {
+    super();
+    this.user = 'André';
+  }
+
+  showUser() {
+    console.log(this.user);
+  }
+}
+
+const MyList = new TodoList();
+
+document.getElementById('newtodo').onclick = function () {
+  MyList.add('New Todo');
+}
+
+MyList.showUser();
+
+class Math {
+  static add(a, b) {
+    return a + b;
+  }
+}
+
+console.log(Math.add(1, 2));
